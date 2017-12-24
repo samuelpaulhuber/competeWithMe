0ba687a85bb1d28cec71b60fc8cd6b5c5ad8f28e
samuelpaulhuber <samhuber84@gmail.com>
2017-12-24 14:58:02 -0500

    c

--- src/components/drill.js
+++ src/components/drill.js
@@ -1,46 +1,29 @@
-// import {React, Component} from 'react';
-// import {connect} from 'react-redux';
-//
-// class Drill extends Component{
-//
-//   render(){
-//     return(
-//       <h3>hi</h3>
-//     );
-//     // if(!this.props.activeDrill) {
-//     //   return (
-//     //     <div>
-//     //       <h3>Loading...</h3>
-//     //     </div>
-//     //   );
-//     // }
-//     // else {
-//     //   return (
-//     //     <div>
-//     //       Drill Info
-//     //       <div>Name: {this.props.activeDrill.name}</div>
-//     //       <div>Image: {this.props.activeDrill.image}</div>
-//     //       <div>Procedure: {this.props.activeDrill.procedure}</div>
-//     //     </div>
-//     //   );
-//     // }
-//   }
-// }
-//
-// function mapStateToProps(state) {
-//   return {
-//     activeDrill: state.activeDrill
-//   };
-// }
-//
-// export default connect(mapStateToProps)(Drill);
 /**
  * Created by samhuber on 7/8/17.
  */
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {Field, reduxForm} from 'redux-form';
+import { renderField } from '../utils/util';
 
 class Drill extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {drill: {}, name: 'turkey', procedure: 'fuck'};
+  }
+
+  getLatestDrill(id){
+    var drill = {
+      name: 'Four Aces',
+      image: 'https://static1.squarespace.com/static/5928efc53a041194d4290832/t/59baf9cfb078698e4757fb93/1505425880621/Double+Reload+Double.png?format=1500w',
+      procedure: `At the standard distance of seven yards, I have selected a 2.5 second goal time. This allows for a 1 second draw, a 1.1 second reload, and two .2 second splits. If you find yourself unable to make that 2.5 second goal time, carefully go through the data from the timer and find the “low hanging fruit”.
+      Now, I feel like I should point out that some people can go much faster than the 2.5 second goal time. It is possible to get a draw time into the .60s and a reload time into the .70s (I have heard some reported times even faster than that). The sky is absolutely the limit on this drill.
+      In order to be competitive in the sport, you don’t need some insane reload time like a .75, but you do need to be able to reload the gun in about 1 second flat on a fairly consistent basis in practice. If you can’t do it, you are going to have a problem.
+      The key to this drill is dryfire training, this is just a “check” for you to make sure you are headed in the right direction.`
+    }
+  }
+
   render(){
     console.log(this.props);
     if(!this.props.activeDrill){
@@ -48,14 +31,71 @@ class Drill extends Component {
         <div>Loading...{this.props.activeDrill}</div>
       );
     }
-    return <div>Loading2...{this.props.activeDrill}</div>;
+
+    let drill = this.getLatestDrill();
+    const divStyle = {
+      marginRight: '5px'
+    };
+    return (
+      <div>
+        <form>
+          <Field
+            //arbitrary property label could be anything!!!
+            label="Name"
+            name="name"
+            component={renderField}
+          />
+          <Field
+            label="Image"
+            name="image"
+            component={renderField}
+          />
+          <Field
+            label="Procedure"
+            name="procedure"
+            component={renderField}
+          />
+          <button type="submit" className="pull-right btn btn-primary">Save</button>
+          <button style={divStyle} className="pull-right btn btn-primary">Back</button>
+        </form>
+      </div>
+    );
   }
 }
 
-function mapStateToProps(state) {
+function validate(values) {
+  const errors = {};
+
+  if (!values.name) {
+    errors.title = 'Enter a Name';
+  }
+  if (!values.image) {
+    errors.categories = 'Enter an Image Link';
+  }
+  if (!values.content) {
+    errors.content = 'Enter the Drill Procedure';
+  }
+}
+  // if we have properties in this object then there are errors
+
+
+  function mapStateToProps(state) {
   return {
-    activeDrill: state.activeDrill
+    activeDrill: state.activeDrill,
+    initialValues:{
+      name: 'Four Aces',
+      image: 'https://static1.squarespace.com/static/5928efc53a041194d4290832/t/59baf9cfb078698e4757fb93/1505425880621/Double+Reload+Double.png?format=1500w',
+      procedure: `At the standard distance of seven yards, I have selected a 2.5 second goal time. This allows for a 1 second draw, a 1.1 second reload, and two .2 second splits. If you find yourself unable to make that 2.5 second goal time, carefully go through the data from the timer and find the “low hanging fruit”.
+      Now, I feel like I should point out that some people can go much faster than the 2.5 second goal time. It is possible to get a draw time into the .60s and a reload time into the .70s (I have heard some reported times even faster than that). The sky is absolutely the limit on this drill.
+      In order to be competitive in the sport, you don’t need some insane reload time like a .75, but you do need to be able to reload the gun in about 1 second flat on a fairly consistent basis in practice. If you can’t do it, you are going to have a problem.
+      The key to this drill is dryfire training, this is just a “check” for you to make sure you are headed in the right direction.`
+    }
   };
 }
 
-export default connect(mapStateToProps)(Drill);
+let drillForm = reduxForm({
+  form: 'ShowDrillForm',
+  enableReinitialize : true,
+  validate
+})(Drill);
+export default connect(mapStateToProps)(drillForm);
